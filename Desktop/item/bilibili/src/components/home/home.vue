d30f9ab5833b4e7819d9e9bf3d5fe4ad6c39199b
kerwin <1742974279@qq.com>
2020-09-16 20:31:05 +0800

    init2

---
+++ Desktop/item/bilibili/src/components/home/home.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <!-- 组件的调用方式<home-header></home-header>或者<HomeHeader/> -->
+        <!-- 显示当前被调用组件页面内的内容 -->
+        <home-header></home-header>
+
+    </div>
+</template>
+
+<script>
+import HomeHeader from './pages/header'
+
+export default {
+  components: {
+    HomeHeader
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ Desktop/item/bilibili/src/components/home/pages/header.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="header">
+      <div class="header-left">
+          <span class="iconfont">&#xe743;</span>
+      </div>
+      <div class="header-seach">
+          <span class="iconfont">&#xe603;</span>
+          输入城市／景点／游玩主题
+      </div>
+      <div class="header-right">
+          北京
+          <span class="iconfont">&#xe600;</span>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped>
+.header {
+    width: 100%;
+    line-height: .88rem;
+    background: #00bcd4;
+    color: #fff;
+    font-size: .36rem;
+    display: flex;
+
+}
+
+</style>
