3e736247bf42a49e00e94f558299a35f434c7449
kerwin <1742974279@qq.com>
2020-09-17 15:09:59 +0800

    add header

--- Desktop/item/bilibili/src/components/home/pages/header.vue
+++ Desktop/item/bilibili/src/components/home/pages/header.vue
@@ -3,8 +3,8 @@
       <div class="header-left">
           <span class="iconfont">&#xe743;</span>
       </div>
-      <div class="header-seach">
-          <span class="iconfont">&#xe603;</span>
+      <div class="header-search">
+          <span class="iconfont">&#xe743;</span>
           输入城市／景点／游玩主题
       </div>
       <div class="header-right">
@@ -18,15 +18,42 @@
 export default {}
 </script>
 
-<style scoped>
+<style lang="stylus" scoped>
+// 引入stylus方式一
+// @import '../../../assets/css/var.styl'
+// 方式二
+@import '~@/assets/css/var.styl'
+
 .header {
     width: 100%;
     line-height: .88rem;
-    background: #00bcd4;
+    background: $bgColor; //  调用stylus中定义的变量
     color: #fff;
     font-size: .36rem;
     display: flex;
 
+}
+.header-left {
+    width: .4rem;
+    padding: 0 .2rem;
+    text-align: center;
+    font-weight: bold;
+}
+.header-search {
+    flex: 1;
+    background: #fff;
+    height: .6rem;
+    margin: .14rem;
+    border-radius: .1rem;
+    color: #e4e7ea;
+    line-height: .6rem;
+    padding-left: .2rem;
+    font-size: .28rem;
+    text-align: left;
+}
+.header-right {
+    font-size: .28rem;
+    padding-right: .2rem;
 }
 
 </style>
