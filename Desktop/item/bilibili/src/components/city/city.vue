329fb8445dc5a3d6cb995c3a6828a8cfddff6a4a
kerwin <1742974279@qq.com>
2020-10-17 16:52:52 +0800

    add hot-city

--- Desktop/item/bilibili/src/components/city/city.vue
+++ Desktop/item/bilibili/src/components/city/city.vue
@@ -1,17 +1,22 @@
 <template>
-    <div>
+    <div class="city">
         <city-header></city-header>
+        <city-hot></city-hot>
     </div>
 </template>
 <script >
 import CityHeader from './pages/header'
+import CityHot from './pages/hot'
 export default {
   components: {
-    CityHeader
+    CityHeader,
+    CityHot
   }
 }
 </script>
 
 <style lang="stylus" scoped>
-
+.city {
+  background : #f5f5f5;
+}
 </style>
---
+++ Desktop/item/bilibili/src/components/city/pages/hot.vue
@@ -0,0 +1,74 @@
+<template>
+    <div class="hot">
+        <div class="sp">热门城市</div>
+        <div>
+            <ul class="hot-list">
+                <li class="hot-item">
+                    上海
+                </li>
+                 <li class="hot-item">
+                    天津
+                </li>
+                 <li class="hot-item">
+                    合肥
+                </li>
+                 <li class="hot-item">
+                    深圳
+                </li>
+                 <li class="hot-item">
+                    广州
+                </li>
+                 <li class="hot-item">
+                    南京
+                </li><li class="hot-item">
+                    北京
+                </li>
+                <li class="hot-item">
+                    上海
+                </li>
+            </ul>
+        </div>
+    </div>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+.hot .sp {
+    font-size: .28rem;
+    text-align: left;
+    color: #212121;
+    margin: .2rem;
+}
+.hot-list {
+    overflow: hidden;
+    /* width: 101%; */
+    background: #fff;
+    position: relative;
+}
+.hot-list:before {
+    content: '';
+    height: 100%;
+    width: 33.333%;
+    right: 33.333%;
+    position: absolute;
+    border-right: solid rgb(177, 175, 175) 1px;
+    border-left: solid rgb(177, 175, 175) 1px;
+}
+.hot-item {
+    color: #212121;
+    font-size: .28rem;
+    text-align: center;
+    height: .5rem;
+    line-height: .5rem;
+    /* border-bottom: rgb(177, 175, 175) solid 1px;
+    border-right: rgb(177, 175, 175) solid 1px;
+    width: 33.09%; */
+    width: 33.333%;
+    border-bottom: solid rgb(177, 175, 175) 1px;
+    float: left;
+}
+
+</style>
